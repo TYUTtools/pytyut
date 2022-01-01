55b8156e1443e8fe658618539af132681669f3c7
jixiaob <1066168689@qq.com>
2022-01-01 15:37:20 +0800

    新增 课表查询接口

--- test.py
+++ test.py
@@ -10,8 +10,15 @@ from pytyut import Pytyut
 
 
 if __name__ == "__main__":
-    # 建议初始化时自己自定义好节点或者使用自动节点选择（校园网结点权重优先）
-    Pytyut.node_link = Pytyut.auto_node_chose(debug=True)
+    Pytyut.node_link = 'https://jxgl20201105.tyutmate.cn/'
+    Pytyut.req_headers_add = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63030532)',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Sec-Fetch-Dest': 'document',
+        'Referer': 'https://helper.tyutmate.cn/tyut/index.html?random=fuckyo',
+    }
     zkg = Pytyut('学号', '教务系统的密码')
     zkg.login(debug=True)
     class_schedule = zkg.get_class_schedule()
